278a9b2bb1b9d27b8f226fefea35e97f57d67aec
vipulchandan <plenteousvipul90009@gmail.com>
2023-09-09 18:09:37 +0530

    created category schema

---
+++ models/CategoryModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
+
+export default mongoose.model("Category", categorySchema);
