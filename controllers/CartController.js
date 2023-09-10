45a20ac9b5ddd961321e8b76a6a6605e8d67d448
vipulchandan <plenteousvipul90009@gmail.com>
2023-09-10 18:05:48 +0530

    completed remove items from cart API

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -255,8 +255,106 @@ const updateCart = async (req, res) => {
 }
 
 
+
+// Remove items from cart
+const removeItemFromCart = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const { productId } = req.body;
+        const userIdFromToken = req.userId;
+
+        // Check if userId is valid
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({
+                status: false,
+                message: "Invalid user id!"
+            });
+        }
+
+        // Check if user exists
+        const user = await UserModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                status: false,
+                message: "User not found!"
+            });
+        }
+
+        // Check if the userId from params matches the userId from token
+        if (user._id.toString() !== userIdFromToken) {
+            return res.status(403).json({
+                status: false,
+                message: "Unauthorized access! You are not the owner of this cart!"
+            });
+        }
+
+        // productId validation
+        if (!productId) {
+            return res.status(400).json({
+                status: false,
+                message: "Product id is required!"
+            });
+        }
+
+        // check if productId is valid
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+            return res.status(400).json({
+                status: false,
+                message: "Invalid product id!"
+            });
+        }
+
+        // check if cart exists
+        const cart = await CartModel.findOne({ userId });
+        if (!cart) {
+            return res.status(404).json({
+                status: false,
+                message: "Cart not found!"
+            });
+        }
+
+        // check if product exists
+        const product = await ProductModel.findOne({ _id: productId, availability: true, isDeleted: false });
+        if (!product) {
+            return res.status(404).json({
+                status: false,
+                message: "Product not found!"
+            });
+        }
+
+        // Check if the Item exists
+        const cartItem = cart.items.find((item) => item.productId.toString() === productId);
+        if (!cartItem) {
+            return res.status(404).json({
+                status: false,
+                message: "Item not found!"
+            });
+        }
+
+        cart.totalItems -= cartItem.quantity;
+        cart.totalPrice -= (cartItem.quantity * product.price);
+
+        cart.items = cart.items.filter((item) => item.productId.toString() !== productId);
+
+        await cart.save();
+
+        res.status(200).json({
+            status: true,
+            message: "Item removed from cart successfully!",
+            data: cart
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+}
+
 export {
     addToCart,
     getCartSummary,
-    updateCart
+    updateCart,
+    removeItemFromCart
 }
--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { 
     addToCart, 
     getCartSummary, 
+    removeItemFromCart, 
     updateCart 
 } from "../controllers/CartController.js";
 import { auth } from "../middlewares/auth.js";
@@ -11,5 +12,6 @@ const router = Router();
 router.post("/users/:userId/cart", auth, addToCart);
 router.get("/users/:userId/cart", auth, getCartSummary);
 router.put("/users/:userId/cart", auth, updateCart);
+router.delete("/users/:userId/cart", auth, removeItemFromCart);
 
 export default router;
