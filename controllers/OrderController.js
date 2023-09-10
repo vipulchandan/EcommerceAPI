ccd0494fc51cf107b1f22e5a73536fa8a6a23b77
vipulchandan <plenteousvipul90009@gmail.com>
2023-09-10 19:42:30 +0530

    Completed fetching Order history API successfully

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -52,7 +52,7 @@ const placeOrder = async (req, res) => {
         }
 
         // Check if cart exists
-        const cart = await CartModel.findOne({ userId });
+        const cart = await CartModel.findById(cartId);
         if (!cart) {
             return res.status(404).json({
                 status: false,
@@ -109,6 +109,54 @@ const placeOrder = async (req, res) => {
 
 
 
+// Order History: Implement an endpoint to fetch the order history for authenticated users
+const getOrderHistory = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const userIdFromToken = req.userId;
+
+        // Check if userId is valid
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({
+                status: false,
+                message: "Invalid user id!"
+            });
+        }
+
+        // Check if user exists
+        const user = await UserModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                status: false,
+                message: "User not found!"
+            });
+        }
+
+        // Check if the userId from params matches the userId from token
+        if (user._id.toString() !== userIdFromToken) {
+            return res.status(403).json({
+                status: false,
+                message: "Unauthorized access! You are not allowed to view order history!"
+            });
+        }
+
+        // find all orders for the user
+        const orders = await OrderModel.find({ userId });
+
+        res.status(200).json({
+            status: true,
+            message: "Order history fetched successfully!",
+            data: orders
+        })
+    } catch(error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+}
+
 export {
-    placeOrder
+    placeOrder,
+    getOrderHistory
 }
--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
-import { placeOrder } from "../controllers/OrderController.js";
+import { 
+    getOrderHistory, 
+    placeOrder 
+} from "../controllers/OrderController.js";
 
 const router = Router();
 
-router.post("/users/:userId/order", auth, placeOrder);
+router.post("/users/:userId/orders", auth, placeOrder);
+router.get("/users/:userId/orders", auth, getOrderHistory);
 
 export default router;
